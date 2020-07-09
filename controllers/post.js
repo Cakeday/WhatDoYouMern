a3a80775c1b235bee9b212c0e3efc52e821cc763
Caleb Reyes <sciencecamp123@gmail.com>
2020-07-08 17:43:25 -0700

    Added post deletion

--- controllers/post.js
+++ controllers/post.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 module.exports = {
 
     getPosts: (req, res) => {
-        Post.find()
+        Post.find().populate("postedBy", "_id name")
             .then(posts => res.json(posts))
             .catch(err => console.log(err))
     },
@@ -16,8 +16,51 @@ module.exports = {
             post.photo.data = req.file.path
             post.photo.contentType = req.file.mimetype
         }
+        post.postedBy = req.profile
         Post.create(post)
             .then(data => res.json(data))
             .catch(error =>res.json(error))
+    },
+
+    postsByUser: (req, res) => {
+        Post.find({postedBy: req.profile._id})
+            .populate("postedBy", "_id name")
+            .sort("created")
+            .then(data => res.json(data))
+            .catch(err => res.status(400).json(err))
+    },
+        
+    postById: (req, res, next, id) => {
+        Post.findById(id)
+            .populate("postedBy", "_id, name")
+            .then(post => {
+                req.post = post
+                next()
+            })
+            .catch(err => res.status(400).json(err))
+    },
+
+    isPoster: (req, res, next) => {
+        let isPoster = req.auth && req.post && req.post.postedBy._id === req.auth._id
+        if (!isPoster) {
+            return res.status(403).json({
+                error: "User is not authorized"
+            })
+        }
+        next()
+    },
+
+
+    deletePost: (req, res) => {
+        let post = req.post
+        Post.deleteOne({_id: post._id})
+        .then(data => res.json({message: "Post deleted successfully"}))
+        .catch(err => res.status(400).json(err))
     }
+
+
+
+
+
+
 }
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPosts, createPost } = require('../controllers/post');
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost } = require('../controllers/post');
 const { createPostValidator } = require('../validator')
 const { requireSignIn } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
@@ -8,9 +8,12 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/post/new/:userId', requireSignIn, createPostValidator, createPost);
+router.get('/posts/by/:userId', requireSignIn, postsByUser);
+router.delete('/post/:postId', requireSignIn, isPoster, deletePost);
 
 // Any route containing a user id will be processed by this middleware
 router.param("userId", userById)
+router.param("postId", postById)
 
 
 module.exports = router
